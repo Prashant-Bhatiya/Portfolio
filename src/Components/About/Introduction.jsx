be842717cdd62b490c4f7961b0c5d3e790def05f
Prashant-Bhatiya <developer.bhatiya@gmail.com>
2022-05-07 11:41:01 +0530

    Changed /Profile Pic

--- src/Components/About/Introduction.jsx
+++ src/Components/About/Introduction.jsx
@@ -13,7 +13,7 @@ export const Introduction = () => {
           <div className="introduction">
             <div className="introduction_logocontainer">
               <img
-                src= "https://i.ibb.co/SQgp7KW/00222-removebg-preview-3.png"
+                src= "https://i.ibb.co/ctHkbDK/0000001-removebg-preview-Cropped.png"
                 alt="Images"
               />
             </div>
